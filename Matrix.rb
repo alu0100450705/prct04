95bec3c646a036b2adbddcf82f311aede34603b8
Marcos Diaz <alu0100450705@ull.edu.es>
2013-10-08 15:52:47 +0100

    multiplicacion arreglada

--- Matrix.rb
+++ Matrix.rb
@@ -25,28 +25,36 @@ def multiplicacion()
 
  
  
- m = Matrix[[1,0],
-			[0,1]]
- m1 = Matrix[[-8,4],
-			 [2,5]]
+ m = Matrix[[2,0,1],
+			[3,0,0],
+			[5,1,1]]
+ m1 = Matrix[[1,0,1],
+ 			 [1,2,1],
+ 			 [1,1,0]]
 
-  mul= Matrix[[0,0],
-			 [0,0]]
+  mul= Matrix[[0,0,0],
+			 [0,0,0],
+			 [0,0,0]]
   puts m
   puts m1
-  puts "La suma seria"
- for k in 0..1
-	for j in 0..1
-		for i in 0..1
-			x =  mul[k,j] 
+  puts "La multiplicacion seria"
+ for k in 0..2
+	for j in 0..2
+	x =  mul[k,j] 
+		for i in 0..2
+		
+			
 			#mul[k,j] =  m[k,i] * m1 [i,j] + x
-			print m[k,i] * m1 [i,j] + x
+			
+			x = m[k,i] * m1 [i,j] + x
 			#mul[k,j] = mul[k,j]+ x
-			 
 			
 		end 
-		puts	
+		print x 
+		print " "
+			
 	end
+	puts
  end
 
 puts
