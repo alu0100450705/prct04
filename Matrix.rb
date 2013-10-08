c6ec7052e0a0ba45977fbeb374195ccd0c5dc34c
CUBILLOS GARCIA <alu3958@cc347.(none)>
2013-10-08 11:37:41 +0100

    suma terminada

--- Matrix.rb
+++ Matrix.rb
@@ -1,2 +1,18 @@
-#suma 
-#mul
+#Para sumar dos matrices ambas deben tener el mismo tamaño
+def suma()
+m = Matrix[[1,0],
+                       [0,1]]
+m1 = Matrix[[-8,4],
+                        [2,5]]
+ puts m
+ puts m1
+ puts "La suma seria"
+for i in 0..1
+       for j in 0..1
+               print m [i,j] + m1[i,j]
+               print " "
+       end
+       puts
+end
+
+end
